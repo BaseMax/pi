0cb27c852b2ab3bf1b150efe835aa4b927b9cf85
Max Base <MaxBaseCode@Gmail.Com>
2020-06-12 12:34:41 +0430

    Update Monte Carlo.c

--- Monte Carlo.c
+++ Monte Carlo.c
@@ -2,3 +2,23 @@
 // Monte-Carlo Calculation of Pi
 // GitHub.com/BaseMax/pi
 // A better and alternative way to create random number: https://github.com/BaseMax/SecureRandStringC
+#include <stdlib.h>
+#include <stdio.h>
+
+double pi(int loop) {
+	int count=0;
+	for(int i=0;i<loop;i++) {
+		double a=(double)rand()/RAND_MAX;
+		double b=(double)rand()/RAND_MAX;
+		if(1>=a*a+b*b)
+			count++;
+	}
+	return (double)count/loop*4;
+}
+
+int main() {
+	int loop=100000; // increase this value to get more digits of PI number
+	srand(10102020); // more secure rand...
+	printf("PI is %g\n", pi(loop));
+	return 0;
+}
